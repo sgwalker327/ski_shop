97a7da47541b5d9900c7d66f5044b9e11c364169
Sam Walker <sg.walker327@gmail.com>
2023-02-04 17:18:23 -0700

    Update model tests with validation tests

--- spec/models/ski_shop_spec.rb
+++ spec/models/ski_shop_spec.rb
@@ -1,5 +1,13 @@
 require "rails_helper"
 
 RSpec.describe SkiShop do
-  it {should have_many :skis}
+  describe 'validations' do
+    it {should validate_presence_of :name}
+    it {should validate_presence_of :location_quantity}
+    it {should validate_presence_of :rents_clothes}
+  end
+
+  describe 'relationships' do
+    it {should have_many :skis}
+  end
 end
