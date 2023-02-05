ddc058782df751aa491b3aec7c4874a34a674964
Sam Walker <sg.walker327@gmail.com>
2023-02-04 20:42:48 -0700

    Edit Ski controller for true/false

--- app/models/ski.rb
+++ app/models/ski.rb
@@ -1,4 +1,4 @@
 class Ski < ApplicationRecord
   belongs_to :ski_shop
-  validates_presence_of :fleet, :manufacturer, :model, :year, :length, :width, :needs_tuned
+  validates_presence_of :fleet, :manufacturer, :model, :year, :length, :width, :needs_tuned, inclusion: [true, false]
 end
