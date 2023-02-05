c5cd975a7868b7464de04fa7ec239f0ed331c7b1
Sam Walker <sg.walker327@gmail.com>
2023-02-04 20:41:40 -0700

    Add routes for ski_shop index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/ski_shop', to: 'ski_shop#index'
+  get '/ski_shop/:id', to: 'ski_shop#show'
   get '/skis', to: 'skis#index'
 end
