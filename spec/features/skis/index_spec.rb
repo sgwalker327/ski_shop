39c40e68fe9627f1c0588b1e031e673c64954bd6
Sam Walker <sg.walker327@gmail.com>
2023-02-05 17:27:52 -0700

    Add test for child button on skis/index_spec

--- spec/features/skis/index_spec.rb
+++ spec/features/skis/index_spec.rb
@@ -22,5 +22,17 @@ RSpec.describe 'The skis index page', type: :feature do
         expect(page).to have_content("Needs Tuned?: #{ski.needs_tuned}")
       end
     end
+
+    describe "When I visit any page on the site" do
+      it 'Then I see a link at the top of the page that takes me to the Child Index' do
+        visit "/skis"
+
+        expect(page).to have_link('Skis', href: skis_path)
+        click_on 'Skis'
+        # save_and_open_page
+        expect(page).to have_current_path('/skis')
+
+      end
+    end
   end
 end
